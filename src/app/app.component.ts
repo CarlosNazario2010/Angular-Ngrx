37be2f1ca1775b9ef5c0a224c79e6c85381f29ce
Carlos Nazario <carlosnazario2010cn@gmail.com>
2025-01-20 21:20:28 -0300

    pequenas alteracoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddIngredientComponent } from "./add-ingredient/add-ingredient.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AddIngredientComponent,
+    ShoppingListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
